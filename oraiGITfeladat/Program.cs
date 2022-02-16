22845a12f60ce6e38b2ac44e2674d5e6c7e06f93
Radeczki Krisztian <radeczki.krisztian@gmail.com>
2022-02-16 12:36:42 +0100

    Add project files.

---
+++ oraiGITfeladat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oraiGITfeladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+            Random random = new Random();
+            int dobaszam = random.Next(6)+1;
+            Console.WriteLine(dobaszam);
+            Console.ReadKey();
+        }
+    }
+}
